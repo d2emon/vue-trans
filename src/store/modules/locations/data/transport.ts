84ceb63d74f62a96ed7cdf3f91ce6613d2425945
Dmitry Kutsenko <d2emonium@gmail.com>
2020-08-12 09:22:42 +0300

    Add transport of 1960

--- src/store/modules/locations/data/transport.ts
+++ src/store/modules/locations/data/transport.ts
@@ -2,7 +2,18 @@ import { TransportData } from '@/store/modules/locations/types';
 import locations, {locationIdBySlug} from './index';
 import transportLinks from './transportLinks';
 
-const buses = [
+const tramIds = [
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+];
+const busIds = [
   '1',
   '2',
   '3',
@@ -18,18 +29,29 @@ const buses = [
   '13',
   '14',
   '15',
-]
+];
 
 const linkIdsByTransportId = (routeId: string): number[] => {
   const slugs: string[] = transportLinks[routeId] || [];
   return slugs.map(locationIdBySlug);
 }
 
-const transport: TransportData[] = buses.map((routeId: string, transportId: number) => ({
+const trams: TransportData[] = tramIds.map((routeId: string, transportId: number) => ({
+  transportId: transportId + 1,
+  transportType: 'Трамвай',
+  routeId,
+  linkIds: linkIdsByTransportId(routeId),
+}));
+const buses: TransportData[] = busIds.map((routeId: string, transportId: number) => ({
   transportId: transportId + 101,
   transportType: 'Автобус',
   routeId,
   linkIds: linkIdsByTransportId(routeId),
 }));
+
+const transport: TransportData[] = [
+  ...trams,
+  ...buses,
+];
 
 export default transport;
